197a8ee9a9a413d922044629fc3089887b078b61
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2023-02-09 12:39:09 +0800

    add huffman coding test

--- test/huffman_test.dart
+++ test/huffman_test.dart
@@ -1,1 +1,19 @@
-void main() {}
+import 'package:test/test.dart';
+
+import 'package:dart_huffman_algorithm/dart_huffman.dart';
+
+void main() {
+  test('Huffman Coding Test', () {
+    final HuffmanCoding coding = HuffmanCoding();
+
+    const String input = 'aaaabbbbcccdde';
+    final String encoded = coding.encode(input);
+    final String decoded = coding.decode(encoded);
+
+    expect(input, decoded);
+
+    final int encodedLength = encoded.length;
+
+    expect(encodedLength, lessThan(input.length * 8));
+  });
+}
