b19f9014faed929493abf56bfbac9c001e29e087
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2023-02-09 12:45:46 +0800

    minor refactor

--- bin/dart_huffman_algorithm.dart
+++ bin/dart_huffman_algorithm.dart
@@ -3,7 +3,7 @@ import 'package:dart_huffman_algorithm/dart_huffman.dart';
 void main(List<String> arguments) {
   final HuffmanCoding huffmanCoding = HuffmanCoding();
 
-  final String encodeResult = huffmanCoding.encode('hamed');
+  final String encodeResult = huffmanCoding.encode(arguments.first);
   print(encodeResult);
 
   final String decodeResult = huffmanCoding.decode(encodeResult);
